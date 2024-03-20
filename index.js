b86c0e87ad4c44ead33025b86b94223c4bccfbfa
genesisplatforms <genesisplatformsinfo@gmail.com>
2024-03-20 15:32:15 -0700

    deply22

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ app.use('/api/casino', require('./routes/Casino/Games'))
 if (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === "production") {
 
     app.use(express.static(path.join(__dirname, 'clinet/build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/client/build/index.html'));
-    })
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    // })
 }
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname +'/client/build/index.html'));
